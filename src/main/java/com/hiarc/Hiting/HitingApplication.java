f7f458fde1cb6243df37f18f3a646b72f39f1009
optiprime27 <gim-yesong@gim-yesong-ui-MacBookPro.local>
2025-02-11 20:38:21 +0900

    Initial commit

---
+++ src/main/java/com/hiarc/Hiting/HitingApplication.java
@@ -0,0 +1,13 @@
+package com.hiarc.Hiting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HitingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hiarc/Hiting/HitingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiarc.Hiting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HitingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
