4e9bc2a4b0aa56627a16250546889cd3641d8254
optiprime27 <yesong0427@gmail.com>
2025-03-13 01:49:16 +0900

    [refac] cors 프런트 도메인 추가

--- src/main/java/com/hiarc/Hiting/global/config/CorsConfig.java
+++ src/main/java/com/hiarc/Hiting/global/config/CorsConfig.java
@@ -14,8 +14,9 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins(
                         "https://hi-rating-front-end.vercel.app",
                         "http://localhost:5173",
-                        "https://hi-rating.co.kr"
-                ) // 허용할 여러 Origin 설정
+                        "https://hi-rating.co.kr",
+                        "https://www.hi-rating.com/"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 인증 정보 허용
